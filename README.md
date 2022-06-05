7986e60a9f7c98d02c77eb668630fdbd8cba0285
whrgo <dev.whrgo@gmail.com>
2022-06-05 19:47:48 +0900

    Add Files

---
+++ README.md
@@ -0,0 +1,14 @@
+# brawlofunweb
+
+Brawl stars Mod.
+
+Made By avarie
+
+Server From Modern-Brawl
+
+Website by avarie :D
+
+This is the Web : https://brawlofun.netlify.app/
+
+Join Brawlofun & Cursed Brawl discord server
+https://discord.gg/A3fppg3PuM
---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+console.log('Script v8')
+console.log('Server v107')
+console.log('Seventh Web Server')
+console.log('UI 2.0 - W3.CSS - BOOTSTRAP - FONT AWESOME')
+function DarkMode() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
